9350fb5911235ae54fa0e54186e723b9224caacf
Alexey M <shadpwkkep@mail.ru>
2021-03-31 18:02:01 +0300

    add slider

---
+++ src/components/SliderVue.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "SliderVue"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,58 @@
+<template>
+    <v-container>
+        <v-carousel>
+            <v-carousel-item
+                    v-for="picture in pictures"
+                    :key="picture"
+            >
+                <v-img :src="'/images/'+picture">
+                    <v-row
+                            class="fill-height"
+                            align="center"
+                            justify="center"
+                    >
+                        <div class="display-3">
+                            Бензин
+                        </div>
+                    </v-row>
+                </v-img>
+            </v-carousel-item>
+        </v-carousel>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            slider:[
+                {
+                    id:1,
+                    img:'Back1.jpg',
+                    title:'Бензин',
+                    subtitle:'bla bla bla',
+                    btn:{
+                        text:"",
+                        cssGradient:''
+                    }
+                }
+            ],
+            pictures: [
+                'Back1.jpg',
+                'Back2.jpg',
+                'Back3.jpg',
+                'Back4.jpg',
+                'Back1.jpg',
+                'Back2.jpg',
+                'Back3.jpg',
+                'Back4.jpg',
+            ],
+        }),
+    }
+</script>
+
+<style lang="scss">
+    $color: red;
+    .test {
+        color: $color;
+    }
+</style>
