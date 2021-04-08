ffd6c12396584da571c0e4a7e577bd5a968bb3f1
Alexey M <shadpwkkep@mail.ru>
2021-04-08 13:28:03 +0300

    'deploy'

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/vue-need-for-drive/' : '/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/vue-need-for-driveMAG/' : '/',
     css: {
         requireModuleExtension: false,
         loaderOptions: {
