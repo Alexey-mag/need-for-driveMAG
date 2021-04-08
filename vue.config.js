8dbc695acba94ad1de3a10493e6b00923dc2d828
Alexey M <shadpwkkep@mail.ru>
2021-04-08 16:29:28 +0300

    add deploy mode

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/vue-need-for-driveMAG/' : '/',
+    // publicPath: '',
+    publicPath: process.env.NODE_ENV === 'production' ? '/need-for-driveMAG/' : '/',
     css: {
         requireModuleExtension: false,
         loaderOptions: {
